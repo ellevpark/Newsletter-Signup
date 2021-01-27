fcac2d2fdd47851a150807b29d98cfaeef7f7ae3
Ellen Park <ellenpark781@gmail.com>
2021-01-27 10:55:53 -0500

    created public folder and sent signup.html to express server

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const app = express(); 
 
+app.use(express.static("public"));
+
 app.listen(3000, function() {
   console.log("I am listening on port 3000")
+})
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html")
 })
