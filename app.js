c12c03ce286e42dccba3c96b657f1e6f963dbf19
Ellen Park <ellenpark781@gmail.com>
2021-01-27 12:12:57 -0500

    created post route to get name and email informaton

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const request = require("request");
 const app = express(); 
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.listen(3000, function() {
   console.log("I am listening on port 3000")
 })
 
+app.post("/", function(req, res) {
+  let firstName = req.body.firstName; 
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  console.log(firstName, lastName, email)
+
+})
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html")
 })
